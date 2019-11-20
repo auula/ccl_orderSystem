10e996a3365b1f199efb754b15da7b2f507fde1e
YooDing <2420498526@qq.com>
2019-11-20 23:03:23 +0800

    updated notes to main application📃

--- order_java/src/main/java/me/ponyo/order/OrderJavaApplication.java
+++ order_java/src/main/java/me/ponyo/order/OrderJavaApplication.java
@@ -3,6 +3,16 @@ package me.ponyo.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author: Ding <br/>
+ * @version: V1.0
+ * @Notes: 程序主启动类
+ * <p>
+ *     Created with IDEA. Date：2019年11月20日22:59:27
+ *     <a href="https://github.com/JDode">to see Github</a>
+ * </p>
+ */
 @SpringBootApplication
 public class OrderJavaApplication {
 
