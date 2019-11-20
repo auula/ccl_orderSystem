3b624f8158514709a73790e712b837e64f1f3e09
YooDing <2420498526@qq.com>
2019-11-20 22:53:36 +0800

    init Java Server project

---
+++ order_java/src/main/java/me/ponyo/order/OrderJavaApplication.java
@@ -0,0 +1,13 @@
+package me.ponyo.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderJavaApplication.class, args);
+    }
+
+}
---
+++ order_java/src/test/java/me/ponyo/order/OrderJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ponyo.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
